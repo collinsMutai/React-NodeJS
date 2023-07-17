9df97774cc932d790d2957ccc73315b0a580c071
Collins Mutai <collinsfrontend@gmail.com>
2023-07-17 18:17:40 +0300

    crud

---
+++ restapi/app.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const multer = require("multer");
+
+const { v4: uuidv4 } = require("uuid");
+
+const path = require("path");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const app = express();
+
+// const fileStorage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, "images");
+//   },
+//   filename: (req, file, cb) => {
+//     cb(null, Date.now().toString() + "-" + file.originalname);
+//   },
+// });
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "images");
+  },
+  filename: function (req, file, cb) {
+    cb(null, uuidv4() + "-" + file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+
+// app.use(bodyParser.urlencoded()) form data
+app.use(bodyParser.json());
+app.use(
+  multer({ storage: storage, fileFilter: fileFilter }).single("image")
+);
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+const feedRoutes = require("./routes/feed");
+
+app.use("/feed", feedRoutes);
+
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500
+  const message = error.message
+  res.status(status).json({message: message})
+})
+
+mongoose
+  .connect(MONGODB_URI)
+  .then((result) => {
+    console.log("connected");
+    app.listen(8080);
+  })
+  .catch((err) => console.log(err));
